d7b9351d46824282150a8155ac20ec8a27db2893
Balamathias <balamathias05@gmail.com>
2024-08-18 10:01:47 +0100

    Fix a UI bug in course-builder.tsx

--- src/pages/course-builder.tsx
+++ src/pages/course-builder.tsx
@@ -10,7 +10,7 @@ const CourseBuilder = () => {
     <div className="flex flex-col min-h-screen">
         <div className="!max-w-7xl flex flex-col items-start justify-start py-8 gap-y-5 max-sm:mx-auto">
             <h2 className="font-bold text-xl md:text-2xl">Create a new Course</h2>
-            <Card onClick={() => setModal(true)} className="p-5 h-60 w-96 rounded-lg flex items-center justify-center bg-background shadow-none border-none cursor-pointer hover:opacity-75">
+            <Card onClick={() => setModal(true)} className="p-5 h-60 md:w-96 w-full rounded-lg flex items-center justify-center bg-background shadow-none border-none cursor-pointer hover:opacity-75">
                 <span className="bg-primary w-8 h-8 flex rounded-full items-center justify-center">
                     <PlusIcon className="text-white" fontSize={18}/>
                 </span>
