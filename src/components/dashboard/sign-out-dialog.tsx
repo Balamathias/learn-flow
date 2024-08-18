1b1f1b676aeb04ea5ed70e264340586f24d9838c
Balamathias <balamathias05@gmail.com>
2024-08-18 10:08:17 +0100

    Fix a UI bug in course-builder.tsx

--- src/components/dashboard/sign-out-dialog.tsx
+++ src/components/dashboard/sign-out-dialog.tsx
@@ -7,7 +7,7 @@ const SignOutDialog = ({onOpenChange, open}: {open?: boolean, onOpenChange?: (bo
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent className="rounded-xl m-2">
+        <DialogContent className="rounded-xl max-[364px]:max-w-[320px]">
             <DialogTitle className="font-bold py-2">Logout?</DialogTitle>
             <DialogDescription>
             You are about to logout of <b className="font-bold">LearnFlow</b>, Do you wish to proceed?
--- src/pages/course-builder.tsx
+++ src/pages/course-builder.tsx
@@ -8,7 +8,7 @@ const CourseBuilder = () => {
   const [openModal, setModal] = useState(false)
   return (
     <div className="flex flex-col min-h-screen">
-        <div className="!max-w-7xl flex flex-col items-start justify-start py-8 gap-y-5 max-sm:mx-auto">
+        <div className="!max-w-7xl flex flex-col items-start justify-start py-8 gap-y-5">
             <h2 className="font-bold text-xl md:text-2xl">Create a new Course</h2>
             <Card onClick={() => setModal(true)} className="p-5 h-60 md:w-96 w-full rounded-lg flex items-center justify-center bg-background shadow-none border-none cursor-pointer hover:opacity-75">
                 <span className="bg-primary w-8 h-8 flex rounded-full items-center justify-center">
@@ -21,8 +21,8 @@ const CourseBuilder = () => {
                   open={openModal}
                   onOpenChange={setModal}
                 >
-                  <DialogContent>
-                    <DialogTitle>Add New Course</DialogTitle>
+                  <DialogContent className="rounded-xl">
+                    <DialogTitle className="py-2 tracking-tight font-semibold">Add New Course</DialogTitle>
                     <AddCourse closeModal={() => setModal(false)} />
                   </DialogContent>
                 </Dialog>
