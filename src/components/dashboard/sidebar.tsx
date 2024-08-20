a95a3245cf650e15cdb8279729dc3562cfefd414
Balamathias <balamathias05@gmail.com>
2024-08-20 16:04:25 +0100

    Clean up

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -47,7 +47,7 @@ const Sidebar = () => {
           links.map(link => (
             <NavLink 
               to={link.href} 
-              className={({isActive}) => cn("flex flex-row items-center p-2 py-2 gap-x-2 rounded-md hover:transition-all hover:bg-secondary hover:duration-300 hover:text-primary px-3.5 relative", {"bg-primary hover:bg-primary/90 hover:opacity-75 hover:transition-all text-gray-100 hover:text-gray-200": isActive && link?.title !== 'Logout'})} 
+              className={({isActive}) => cn("flex flex-row items-center p-2 py-2 gap-x-2 rounded-md hover:transition-all hover:duration-300 hover:text-primary px-3.5 relative hover:bg-secondary", {"bg-primary hover:bg-primary/90 hover:opacity-75 hover:transition-all text-gray-100 hover:text-gray-200": isActive && link?.title !== 'Logout'})} 
               key={link.title}
               end={link?.href === '/dashboard'}
               onClick={link?.title === 'Logout' ? (e) => {
